9f736a33a04fb903dc9a70f65da692db8df2cbbb
phil7690 <phillipskat@gmail.com>
2018-06-22 15:55:13 -0400

    CSV multi line input.

--- startfile1.rb
+++ startfile1.rb
@@ -1,5 +1,7 @@
 require 'csv'
 
+set_file = "set_pieces.csv"
+
 #  if user input = flat build .new from class.
 # or can you do user input = class x build .new from class???
 print "Need a name: "
@@ -10,7 +12,8 @@ user_input = gets.chomp
 user_input.downcase!
 
 if user_input.include? "flat"
-  File.open(name+ "_" + user_input + ".csv", "w") do |line|
+# this line is broken. I just want it to write to the set_file
+open(set_file, 'a') do |line|
     puts "Enter width: "
     width = gets.chomp
     puts "Width entered: #{width}"
@@ -23,11 +26,24 @@ if user_input.include? "flat"
     puts "Enter Finish (painted, bare, etc.): "
     finish = gets.chomp
     puts "Finish entered: #{finish}"
-    line.puts width, height, style, finish
+    line.print name, ",", "flat,", width,",", height,",", style,",", finish, ",","\n"
   end
 elsif user_input.include? "riser"
-  puts "gonna build a riser.."
-
+  open(set_file, 'a') do |line|
+    puts "Enter width: "
+    width = gets.chomp
+    puts "Width entered: #{width}"
+    puts "Enter depth: "
+    depth = gets.chomp
+    puts "Depth entered: #{depth}"
+    puts "Enter height: "
+    height = gets.chomp
+    puts "Height entered: #{height}"
+    puts "Enter Finish (painted, bare, etc.): "
+    finish = gets.chomp
+    puts "Finish entered: #{finish}"
+    line.print name,",","riser,", width,",", depth,",", height,",", finish, ",", "\n"
+  end
 else
   # eventually this will be an option to create a new class
   print "type not found"
@@ -43,7 +59,7 @@ module Contact
 
   end
 # should have address, pick up/deliver, return status
-  def info
+  def info(text)
     @address = address
     @phone = phone
     @email = email
@@ -54,28 +70,20 @@ module Contact
 
 
 # things that don't fit anywhere else
-  def notes
+  def notes(text)
     @other = other
   end
 
 end
 
-
+# https://stackoverflow.com/questions/42891288/creating-a-class-based-on-user-input
 
 class Flat
   include Contact
-
-  attr_reader :width, :height, :style, :finish
-
-  def initialize(width, height, style, finish)
-    @width = width
-  end
-
-  def description
-    puts "This is a #{size} #{style} flat."
-  end
-
+  attr_accesor :width, :height, :style, :finish
 end
+
+flats =
 
 class Riser
 end
@@ -89,21 +97,3 @@ end
 
 class Stands
 end
-
-
-
-# File.open("flat_input.csv", "w") do |line|
-#puts "Enter width: "
-#width = gets.chomp
-#puts "Width entered: #{width}"
-#puts "Enter height: "
-#height = gets.chomp
-#puts "Height entered: #{height}"
-#puts "Enter style (TV or Theatre): "
-#style = gets.chomp
-#puts "Style entered: #{style}"
-#puts "Enter Finish (painted, bare, etc.): "
-#finish = gets.chomp
-#puts "Finish entered: #{finish}"
-#line.puts width, height, style, finish
-#end
